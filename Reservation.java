4599e7795ee1f093d3430747f199a49815c75fb3
jph432 <johnhealey1997@hotmail.com>
2016-07-14 20:59:46 -0230

    Add comments/documentation

--- Reservation.java
+++ Reservation.java
@@ -1,12 +1,19 @@
 package reservations;
 
+/**
+ * A class which models a reservation at a hotel, including all information about the guest, the check-in and 
+ * check-out dates, and the information about the room the customer wants.
+ */
+
 import java.util.ArrayList;
 
-//ADD DATES AND SHIT
 public class Reservation {
 
-	//-----------------------------------------------------------------------------------------------------------------------------------
-	private Room[][] hotel = new Room[16][21];
+	//Attributes----------------------------------------------------------------------------------------------------------------------------
+	
+	private final int NUM_FLOORS = 15;
+	private final int ROOMS_PER_FLOOR = 20;
+	private Room[][] hotel = new Room[NUM_FLOORS][ROOMS_PER_FLOOR];  //A model of a hotel for reference. 15 floors with 20 rooms per floor.
 	
 	private String name;
 	private String phoneNum;
@@ -16,27 +23,27 @@ public class Reservation {
 	private int checkInMonth; 
 	private String checkInMonthString;
 	private int checkInYear; 
-	private String checkInString;
+	private String checkInString;	//A string modelling the check-in date, in the format of "DD MM YYYY"
 	private int checkOutDay; 
 	private String checkOutDayString;
 	private int checkOutMonth; 
 	private String checkOutMonthString;
 	private int checkOutYear; 
-	private String checkOutString;
+	private String checkOutString;   //A string modelling the check-in date, in the format of "DD MM YYYY"
 	private int adultsInRoom;
 	private final int COST_PER_ADULT = 50;
 	private int childrenInRoom;
 	private final int COST_PER_CHILD = 30;
 	
-	//private String desiredSize;
-	private int desiredNumBeds;
+	//private String desiredSize; This attribute was removed, as it was redundant to have room size and number of beds, 
+	private int desiredNumBeds;   //since the number of beds decides the size of the room.
 	private boolean wantsView;
-	private String wantsViewString;
+	private String wantsViewString;  //Used in the toString() method, simply adds "Wants a view" to the descrption.
 		
 	private double roomCost;
 	private Room toReserve;
 	
-	//---------------------------------------------------------------------------------------------------------------------------------------
+	//Constructor Method---------------------------------------------------------------------------------------------------------------------
 	
 	public Reservation(String n, String pN, String cC, int cID, int cIM, int cIY, int cOD, int cOM, int cOY, int aIR, int cIR, 
 			int dNB, boolean wV) {
@@ -54,24 +61,26 @@ public class Reservation {
 		//desiredSize = dS;
 		desiredNumBeds = dNB;
 		wantsView = wV;
-		for (int i = 1; i < 16; i++) {
-			for (int j = 0; j < 21; j++) {
-				String roomNumberString = i + "";
+		//The initialization of the hotel 2-D Array. You can ignore, but for the record
+		//the first 6 floors have single rooms, 7-11 doubles, 12-14 suites, 15 is the penthouse.
+		for (int floor = 1; floor <= NUM_FLOORS; floor++) {
+			for (int room = 0; room <= ROOMS_PER_FLOOR; room++) {
+				String roomNumberString = floor + "";
 				int roomNumber;
 				int numBeds;
 				//String size;
 				boolean hasView;
-				if (i == 15) {
+				if (floor == 15) {
 					numBeds = 4;
 					//size = "penthouse";
 					hasView = true;
 				}
-				else if (i > 11) {
+				else if (floor > 11) {
 					numBeds = 3;
 					//size = "large";
 					hasView = true;
 				}
-				else if (i > 5) {
+				else if (floor > 5) {
 					numBeds = 2;
 					//size = "medium";
 					hasView = true;
@@ -81,19 +90,19 @@ public class Reservation {
 					//size = "small";
 					hasView = false;
 				}
-				if (j < 10) {
-					roomNumberString += "0" + j;
+				if (room < 10) {
+					roomNumberString += "0" + room;
 				}
 				else {
-					roomNumberString += j;
+					roomNumberString += room;
 				}
 				roomNumber = Integer.parseInt(roomNumberString);
-				hotel[i][j] = new Room(roomNumber, numBeds, hasView);
+				hotel[floor][room] = new Room(roomNumber, numBeds, hasView);
 			}
 		}
 	}
 	
-	//-------------------------------------------------------------------------------------------------------------------------------------
+	//Accessor Methods-------------------------------------------------------------------------------------------------------------------------
 	
 	public String getName() {
 		return name;
@@ -158,7 +167,7 @@ public class Reservation {
 		return roomCost;
 	}
 	
-	//-----------------------------------------------------------------------------------------------------------------------------------
+	//Mutator Methods-------------------------------------------------------------------------------------------------------------------------
 	
 	public void setName(String newName) {
 		name = newName;
@@ -205,7 +214,7 @@ public class Reservation {
 	}
 	
 	/**
-	public void setDesiredSize(String newDesiredSize) {
+	public void setDesiredSize(String newDesiredSize) {  
 		desiredSize = newDesiredSize;
 	}
 	*/
@@ -218,24 +227,29 @@ public class Reservation {
 		wantsView = newWantsView;
 	}
 	
-	//------------------------------------------------------------------------------------------------------------------------------------
+	//Other Methods-------------------------------------------------------------------------------------------------------------------------
 	
+	//Searches the sample hotel Array for a room with the given desired features.
+	//This would have to be modified to accomodate the floors and rooms/per floor of the used hotel. (edit attributes)
 	public ArrayList<Room> searchForRoom() {
 		ArrayList<Room> options = new ArrayList<>();
-		for (int i = 1; i < 16; i++) {
-			for (int j = 1; j < 21; j++) {
-				if (hotel[i][j].getNumBeds() == desiredNumBeds && 
+		for (int floor = 1; floor <= NUM_FLOORS; floor++) {
+			for (int room = 1; room <= ROOMS_PER_FLOOR; room++) {
+				if (hotel[floor][room].getNumBeds() == desiredNumBeds && 
 					//hotel[i][j].getSize().equals(desiredSize) && 
-					hotel[i][j].getHasView() == wantsView && 
-					hotel[i][j].isVacant(checkInString, checkOutString)) { 
-						options.add(hotel[i][j]);
+					hotel[floor][room].getHasView() == wantsView && 
+					hotel[floor][room].getIsVacantBetween(checkInString, checkOutString)) { 
+						options.add(hotel[floor][room]);
 				}
 			}
 		}
 		return options;
 	}
 	
+	//Relies fairly heavily on the reserve(String checkIn, String checkOut) method of the room class.
+	//Books the room with the given number from the check-in date to the check-out date (both are attributes).
 	public void reserve(int roomNumber) {
+		//Lines 252-264 are just finding the given room in the hotel Array.
 		String numberString = roomNumber + "";
 		String floorString = "";
 		String roomString = "";
@@ -250,19 +264,21 @@ public class Reservation {
 		int floor = Integer.parseInt(floorString);
 		int room = Integer.parseInt(roomString);
 		toReserve = hotel[floor][room];
-		checkInString = checkInDayString + " " + checkInMonthString + " " + checkInYear;
-		checkOutString = checkOutDayString + " " + checkOutMonthString + " " + checkOutYear;
+		checkInString = checkInDayString + " " + checkInMonthString + " " + checkInYear; //Dates use this format (DD MM YYYY) to make
+		checkOutString = checkOutDayString + " " + checkOutMonthString + " " + checkOutYear; //reading from a text file potentially easier
 
-		toReserve.reserve(checkInString, checkOutString);
-		System.out.println("Room Reserved");
+		toReserve.reserve(checkInString, checkOutString); //Note: does not check room vacany as only rooms already known to be available
+		System.out.println("Room Reserved");				//are presented to the user in the GUI.
 	}
 	
+	//Updates the roomCost attribute given all the information about the reservation.
 	public void calcRoomCost() {
 		double roomRate = (double)toReserve.getRoomRate();
 		double peopleRate = (double)(adultsInRoom * COST_PER_ADULT + childrenInRoom * COST_PER_CHILD);
 		roomCost = roomRate + peopleRate;
 	}	
 	
+	//Properly formats the check-in and check-out dates by placing a 0 before single-digit dates.
 	public void formatDates() {
 		checkInDayString = "";
 		checkOutDayString = "";
@@ -297,6 +313,7 @@ public class Reservation {
 			wantsViewString = "Wants a view.";
 	}
 	
+	//Returns all the information about the reservation in a well-formatted String
 	public String toString() {
 		formatDates();
 		return "Name: " + name + "\n" + 
@@ -309,5 +326,4 @@ public class Reservation {
 				"Number of beds: " + desiredNumBeds + "\n" + 
 				wantsViewString;
 	}
-	
 }
